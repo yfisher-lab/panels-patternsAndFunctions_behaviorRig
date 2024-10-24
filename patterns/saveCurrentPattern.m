0d086c80afaa5861108c3e1c69f68a042ef14397
Andrew <andrewhill@berkeley.edu>
2024-10-23 17:18:34 -0700

    Fixed makeBarPattern
    Code now creates bar patterns that wrap around and straddle the 'gap' behind the fly (whereas before it would disappear for col positions 89-96). Can be modified for different bar widths and for dark-on-bright scenes.

---
+++ patterns/saveCurrentPattern.m
@@ -0,0 +1,4 @@
+directory_name =...
+    'C:\Users\fisherlab\Documents\GitHub\panels-patternsAndFunctions_behaviorRig\patterns\';
+str = [directory_name 'Pattern_008_2pxBrightBar.mat']; 	% name must begin with Pattern_
+save(str, 'panel_pattern');
